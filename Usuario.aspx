871d2c5e1fdf5d0aed796b57b35048bc6c2f38cc
Akue21 <ana.karina.akue@gmail.com>
2017-06-12 11:09:09 -0500

    cambios_usuario

---
+++ Usuario.aspx
@@ -0,0 +1,174 @@
+﻿ <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Usuario.aspx.cs" Inherits="Usuario" UnobtrusiveValidationMode="None"%>
+
+<!DOCTYPE html>
+<script runat="server">
+
+</script>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Document</title>
+        <style>
+            body{
+                margin:0;
+                text-align: center;
+                background: #A1887F;
+                color:#F5F5F5;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            }
+            
+            div.titulo{
+                margin: 0;
+                line-height: 100px;
+                background: #AED581;
+                border-width: 10px;
+                border-style:groove;
+            }
+            
+            div.titulo h1{
+                margin: 0;
+            }
+            
+            div.titulo img{
+                width: 100px;
+                height: 100px;
+                float: left;
+                margin-left: 150px;
+            }
+            
+            div.table{
+                width:100%;
+                display: table;
+                height: 465px
+            }
+            
+            div.cell{
+                display: table-cell;
+                border-width: 7px;
+                border-style:groove;
+                border-color:#AED581;
+                position: relative;
+            }
+            
+            div.cell:nth-child(2){
+                width: 80%;
+            }
+            
+            ul{
+                list-style: none;
+                padding: 0;
+            }
+            
+            li::before{
+                content: "★";
+            }
+            
+            div.pie{
+                line-height: 70px;
+                background: #AED581;
+            }
+            
+            div.pie p{
+                margin: 0;
+            }
+            
+            div.cell img{
+                margin: 0 auto;
+                display: block;
+            }
+            
+            div.cell > table{
+                display: inline-block;
+            }
+            
+            div.grid{
+                height:200px;
+                width:650px;
+                border:dashed;
+                display: inline-block;
+            }
+            
+            div.menu{
+                position: absolute;
+                top:0;
+            }
+            
+           div.cell:nth-child(2){
+               padding-top:50px;
+            }
+            
+            h1{
+                width: 80%;
+                margin-left: 800px;
+            }
+        </style>
+    </head>
+<body>
+    <form id="form1" runat="server">
+    <div class="titulo">
+          &nbsp;<h1>★ USUARIOS ★</h1>
+       </div>
+        <div class="table">
+            <div class="cell">
+            </div>
+            <div class="cell">
+                <table>
+                   <tr>
+                        <td>Id Usuario</td>
+                        <td><asp:TextBox ID="id" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator6" runat="server" ErrorMessage="Campo necesario" ControlToValidate="id" ValidationGroup="grupoELI"></asp:RequiredFieldValidator></td>
+                    </tr>
+                    <tr>
+                        <td>Nombre</td>
+                        <td><asp:TextBox ID="nombre" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ErrorMessage="Campo necesario" ControlToValidate="nombre" ValidationGroup="grupoAE"></asp:RequiredFieldValidator></td>
+                    </tr>
+                    <tr>
+                        <td>Apellido paterno</td>
+                        <td><asp:TextBox ID="app" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ErrorMessage="Campo necesario" ControlToValidate="app" ValidationGroup="grupoAE"></asp:RequiredFieldValidator></td>
+                    </tr>
+                    <tr>
+                        <td>Apellido materno</td>
+                        <td><asp:TextBox ID="apm" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="Campo necesario" ControlToValidate="apm" ValidationGroup="grupoAE"></asp:RequiredFieldValidator></td>
+                    </tr>
+                    <tr>
+                        <td>Sucursal</td>
+                        <td><asp:DropDownList ID="sucursal" runat="server"></asp:DropDownList></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator7" runat="server" ErrorMessage="Campo necesario" InitialValue=""  ControlToValidate="sucursal" ValidationGroup="grupoAE"></asp:RequiredFieldValidator></td>
+                    </tr>
+                    <tr>
+                        <td>Alias</td>
+                        <td><asp:TextBox ID="alias" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Campo necesario" ControlToValidate="alias" ValidationGroup="grupoAE"></asp:RequiredFieldValidator></td>
+                    </tr>
+                    <tr>
+                        <td>Contraseña</td>
+                        <td><asp:TextBox ID="pass" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Campo necesario" ControlToValidate="pass" ValidationGroup="grupoAE"></asp:RequiredFieldValidator></td>
+                    </tr>
+                </table>
+                <div class="grid">
+                    <asp:GridView ID="GridView1" runat="server" AllowPaging="True" PageSize="5" OnPageIndexChanging="OnPageIndexChanging" AutoGenerateSelectButton="True" OnSelectedIndexChanged="SelectNdx"></asp:GridView>
+                </div>
+                <div class="obciones">
+                    <asp:Button ID="buscar" runat="server" Text="buscar" OnClick="buscar_Click" CausesValidation="false"/>
+                    <asp:Button ID="agregar" runat="server" Text="agregar" OnClick="agregar_Click" ValidationGroup="grupoAE"/>
+                    <asp:Button ID="eliminar" runat="server" Text="eliminar" OnClick="eliminar_Click" ValidationGroup="grupoELI"/>
+                    <asp:Button ID="modificar" runat="server" Text="modificar" OnClick="modificar_Click" ValidationGroup="grupoAE"/>
+                    <asp:Button ID="limpiar" runat="server" Text="limpiar" OnClick="limpiar_Click" CausesValidation="false"/>
+                </div>
+                <div class="mensage">
+                    <asp:Label ID="mensage" runat="server" Text=""></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="pie">
+            <p>&nbsp;</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Usuario.aspx.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace ExamenAdriansilloEfraFinal
+{
+    public partial class Usuario : System.Web.UI.Page
+    {
+		
+        MySqlConnection con = new MySqlConnection("server=64.62.211.131; Database=efrain_basenube; Uid=efrain_efrain; pwd=123456789efra; ");
+ 
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                con.Open();
+
+                MySqlCommand consultar = con.CreateCommand();
+                consultar.CommandText = "Select * From sucursal;";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(consultar);
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                sucursal.DataTextField = "nom_suc";
+                sucursal.DataValueField = "id_suc";
+                sucursal.DataSource = dt;
+                sucursal.DataBind();
+               sucursal.Items.Insert(0, new ListItem("", ""));
+                con.Close();
+            }
+        }
+
+        protected void buscar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (sucursal.Text == "0")
+                {
+                    sucursal.Text = "";
+                }
+
+                con.Open();
+                MySqlCommand consulta = con.CreateCommand();
+                consulta.CommandText = "SELECT id_usr, nom_usr, app_usr, apm_usr, alias_usr, nom_suc, psw_usr " +
+                                       "FROM usuarios, sucursal WHERE nom_usr LIKE '" + nombre.Text + "%' AND " +
+                                                                    "app_usr LIKE '" + app.Text + "%' AND " +
+                                                                    "apm_usr LIKE '" + apm.Text + "%' AND " +
+                                                                    "Sucursal_id_suc LIKE '" + sucursal.Text + "%' AND " +
+                                                                    "alias_usr LIKE '" + alias.Text + "%' AND " +
+                                                                    "id_suc = Sucursal_id_suc";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(consulta);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch (MySqlException)
+            {
+                mensage.Text = "Hubo un error compa";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void limpiar_Click(object sender, EventArgs e)
+        {
+            id.Text = "";
+            nombre.Text = "";
+            alias.Text = "";
+            app.Text = "";
+            apm.Text = "";
+            pass.Text = "";
+            sucursal.SelectedIndex = sucursal.Items.IndexOf(sucursal.Items.FindByText(""));
+        }
+
+        protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            buscar_Click(sender, e);
+        }
+
+        protected void SelectNdx(object sender, EventArgs e)
+        {
+            GridViewRow row = GridView1.SelectedRow;
+            id.Text = System.Net.WebUtility.HtmlDecode(row.Cells[1].Text);
+            nombre.Text = System.Net.WebUtility.HtmlDecode(row.Cells[2].Text);
+            app.Text = System.Net.WebUtility.HtmlDecode(row.Cells[3].Text);
+            apm.Text = System.Net.WebUtility.HtmlDecode(row.Cells[4].Text);
+            alias.Text = System.Net.WebUtility.HtmlDecode(row.Cells[5].Text);
+            string nom = System.Net.WebUtility.HtmlDecode(row.Cells[6].Text);
+            sucursal.SelectedIndex = sucursal.Items.IndexOf(sucursal.Items.FindByText(nom));
+            pass.Text = System.Net.WebUtility.HtmlDecode(row.Cells[7].Text);
+        }
+
+        protected void agregar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+
+                //INSERTAMOS EL VALOR
+                cmd.CommandText = "INSERT INTO usuarios (nom_usr,app_usr,apm_usr,Sucursal_id_suc,alias_usr,psw_usr) " +
+                "VALUES('" + nombre.Text + "','" + app.Text + "','" + apm.Text + "'," + sucursal.Text + ",'" + alias.Text + "','" + pass.Text + "')";
+                cmd.ExecuteNonQuery();
+                
+                mensage.Text = "Se inserto bien!";
+            }
+            catch (MySqlException)
+            {
+                mensage.Text = "hay un error";
+            }
+            finally
+            {
+                con.Close();
+                actualizarData();
+            }
+        }
+
+        protected void actualizarData()
+        {
+            
+
+            con.Open();
+            MySqlCommand consulta = con.CreateCommand();
+            consulta.CommandText = "SELECT id_usr, nom_usr, app_usr, apm_usr, alias_usr, nom_suc, psw_usr " +
+                                   "FROM usuarios, sucursal WHERE id_suc = Sucursal_id_suc";
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(consulta);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void eliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "DELETE FROM usuarios WHERE id_usr = '" + id.Text + "'";
+                cmd.ExecuteNonQuery();
+                
+                mensage.Text = "Se elimino";
+                  
+            }
+            catch (MySqlException)
+            {
+                mensage.Text = "ah ocurrido un error";
+            }
+            finally
+            {
+                con.Close();
+                actualizarData();
+            }
+        }
+
+        protected void modificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+
+                //ACTUALIZANDO EL VALOR
+                cmd.CommandText = "UPDATE usuarios SET nom_usr='" + nombre.Text + "', app_usr='" + app.Text + "',"
+                                                    + "apm_usr='" + apm.Text + "',Sucursal_id_suc=" + sucursal.Text + ","
+                                                    + "alias_usr='" + alias.Text + "',psw_usr='" + pass.Text + "' "
+                                + "WHERE id_usr = '" + id.Text + "'";
+                cmd.ExecuteNonQuery();
+
+                mensage.Text = "Se ah modificado";
+                 
+            }
+            catch (MySqlException)
+            {
+                mensage.Text = "hubo un error";
+            }
+            finally
+            {
+                con.Close();
+                actualizarData();
+            }
+        }
+    }
+}
