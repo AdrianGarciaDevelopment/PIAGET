a12b5aaeb2edbcaca5373e51bf59a1800386bfc6
luisdanielmata93 <ld.dominguez.mata@gmail.com>
2017-06-12 09:25:48 -0500

    Subido por LDDM
    Derechos reservador LDDM

---
+++ RegistrarSucursal.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RegistrarSucursal : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnRegistrarSucursal_Click(object sender, EventArgs e)
+    {
+        int id = Convert.ToInt16( txtID.Text);
+        string nombre = txtNombre.Text;
+        string direccion = txtDireccion.Text;
+        string telefono = txtTelefono.Text;
+        Consultas.setSucursal(id, nombre, direccion, telefono, lblRES);
+    }
+}
